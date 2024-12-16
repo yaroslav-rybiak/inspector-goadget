be8d16ca5a0a344b90c79eb4d006b725c2d009c2
Yaroslav Rybiak <yaroslav.rybiak@gov.ab.ca>
2024-12-15 20:53:40 -0700

    Added functions to index

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,9 @@
 declare module "inspector-goadget" {
+  export function clickGoaButton(buttonText: string): void;
   export function typeInGoaInput(label: string, text: string): void;
+  export function selectGoaDropdownOption(
+    dropdownLabel: string,
+    optionText: string
+  ): void;
   export function uploadFileToGoaInput(label: string, fileName: string): void;
 }
